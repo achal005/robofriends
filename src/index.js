c58d0dda7e8ebdaacad0150aa32050d9ace8d217
achal005 <achal005@fiu.edu>
2020-05-03 11:14:38 -0400

    updated project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import App from "./containers/App";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +12,9 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+
+// reactDOM.render(<Card />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
